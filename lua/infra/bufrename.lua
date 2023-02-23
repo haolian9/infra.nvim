f071c161ef4349db711685862a731e279ad2d703
haoliang <haoliang0.1.2@gmail.com>
2023-02-23 21:21:14 +0800

    initial commit

---
+++ lua/infra/bufrename.lua
@@ -0,0 +1,26 @@
+local unsafe = require("infra.unsafe")
+local jelly = require("infra.jellyfish")("infra.bufrename")
+
+local api = vim.api
+
+---@param bufnr number
+---@param full_name string @absolute path for buffer
+---@param short_name string|nil @when nil then take from full_name
+---@return boolean
+return function(bufnr, full_name, short_name)
+  bufnr = bufnr or api.nvim_get_current_buf()
+  assert(full_name ~= nil)
+
+  local modified = api.nvim_buf_get_option(bufnr, "modified")
+  local ok = unsafe.setfname(bufnr, full_name, short_name)
+  if ok then
+    -- increase the &changedtick
+    unsafe.unchanged(bufnr, false, true)
+    -- and restore the &modified
+    if modified then api.nvim_buf_set_option(bufnr, "modified", modified) end
+  else
+    jelly.err("failed to rename %d to %s", bufnr, full_name)
+  end
+
+  return ok
+end
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+I hate including dependencies into nvim plugins, and tried hard to avoid it but
+eventually found I have to have one.
+
+status: it is not well tested, use it at your own risk.
