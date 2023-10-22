d101b3ae4d48ad0ce67825f74e309e214e32d5af
haoliang <haoliang0.1.2@gmail.com>
2023-10-22 19:46:26 +0800

    sync with upstream

--- lua/infra/utf8.lua
+++ lua/infra/utf8.lua
@@ -11,34 +11,33 @@ local ranges = {
 M.maxbytes = #ranges
 
 ---@param chars string
----@param offset ?number @default to 1
+---@param offset? number @default to 1
 ---@return number
-M.byte0 = function(chars, offset) return string.byte(chars, offset or 1) end
+function M.byte0(chars, offset) return string.byte(chars, offset or 1) end
 
 ---@param byte0 number
-M.rune_length = function(byte0)
+function M.rune_length(byte0)
   for i, range in ipairs(ranges) do
     if byte0 >= range[1] and byte0 <= range[2] then return i end
   end
   error("invalid utf8 start byte")
 end
 
--- iterate over utf8 runes
+--iterate over utf8 runes
 ---@param chars string @chars
-M.iterate = function(chars)
+function M.iterate(chars)
   local offset = 1
 
   ---@return nil|string
   return function()
     if offset > #chars then return end
 
-    local _len = M.rune_length(M.byte0(chars, offset))
+    local len = M.rune_length(M.byte0(chars, offset))
+    local last = offset + len - 1
+    assert(#chars >= last, "not enough bytes for a utf8 rune")
 
-    local _end = offset + _len - 1
-    assert(#chars >= _end, "not enough bytes for a utf8 rune")
-
-    local rune = chars:sub(offset, _end)
-    offset = _end + 1
+    local rune = chars:sub(offset, last)
+    offset = last + 1
 
     return rune
   end
