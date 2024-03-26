597eefd9f19a160751bb791bd9ccd1d38d58c57b
haoliang <haoliang0.1.2@gmail.com>
2024-03-26 17:25:32 +0800

    sync with upstream

--- lua/infra/unsafe.lua
+++ lua/infra/unsafe.lua
@@ -15,27 +15,39 @@ local api = vim.api
 ffi.cdef([[
 // nvim specific
 
+  // nvim/getchar.c
   // kudos to ii14: https://matrix.to/#/!JAfPjWAdLCtgeCAwnS:matrix.org/$1662254787169324naFSw:matrix.org?via=matrix.org&via=libera.chat&via=gitter.im
   unsigned char *get_inserted(void);
 
-  // see memory.c
+  // nvim/memory.c
   void xfree(void * p);
   char *xstrdup(const char *str);
 
-  // see buffer_defs.h
+  // nvim/buffer_defs.h
   typedef void *buf_T;
   buf_T *buflist_findnr(int nr);
   int setfname(buf_T *buf, char *ffname, char *sfname, bool message);
 
-  // change.c
+  // nvim/change.c
   void unchanged(buf_T *buf, int ff, bool always_inc_changedtick);
 
-  // see nvim/memline.c
+  // nvim/memline.c
   char *ml_get_buf(buf_T *buf, int32_t lnum, bool will_change);
   size_t strlen(const char *str);
 
-  // help.c
+  // nvim/help.c
   void prepare_help_buffer(void);
+
+  // nvim/cmdhist.c
+  typedef void *list_T;
+  typedef struct {
+    int hisnum;                       // Entry identifier number.
+    char *hisstr;                     // Actual entry, separator char after the NUL.
+    uint64_t timestamp;               // Time when entry was added.
+    list_T *additional_elements; // Additional entries from ShaDa file.
+  } histentry_T;
+  const void *hist_iter(const void *const iter, const uint8_t history_type, const bool zero, histentry_T *const hist);
+
 
 // sys
 
@@ -48,9 +60,15 @@ ffi.cdef([[
 
 local C = ffi.C
 
----@diagnostic disable-next-line
-local FAIL = 0
-local OK = 1
+local nvim_enum = {
+  FAIL = 0,
+  OK = 1,
+  HIST_CMD = 0,
+}
+
+local c_enum = {
+  WNOHANG = 1,
+}
 
 ---@return string|nil
 function M.get_inserted()
@@ -58,8 +76,6 @@ function M.get_inserted()
   if p == nil then return end
   return ffi.string(p)
 end
-
-local WNOHANG = 1
 
 function M.WIFEXITED(wstatus) return bit.band(wstatus, 0x7f) == 0 end
 
@@ -76,7 +92,7 @@ function M.waitpid(pid, nohang)
   if nohang == nil then nohang = false end
 
   local status = ffi.new("int[1]")
-  local waited_pid = C.waitpid(pid, status, nohang and WNOHANG or 0)
+  local waited_pid = C.waitpid(pid, status, nohang and c_enum.WNOHANG or 0)
   return waited_pid, tonumber(status[0])
 end
 
@@ -95,7 +111,7 @@ function M.setfname(bufnr, full_name, short_name)
   local sfname
   if short_name ~= nil then sfname = C.xstrdup(short_name) end
 
-  return C.setfname(buf_p, ffname, sfname, true) == OK
+  return C.setfname(buf_p, ffname, sfname, true) == nvim_enum.OK
 end
 
 ---@param bufnr number
@@ -145,5 +161,24 @@ end
 ---@param fd number
 ---@return boolean
 function M.isatty(fd) return C.isatty(fd) == 1 end
+
+---@return fun(): string?
+function M.hist_iter()
+  local done = false
+  local iter = ffi.new("const void *")
+  local entry = ffi.new("histentry_T")
+
+  return function()
+    if done then return end
+
+    iter = C.hist_iter(iter, nvim_enum.HIST_CMD, false, entry)
+    if iter == nil then
+      done = true
+    else
+      assert(entry.hisstr ~= nil)
+      return ffi.string(entry.hisstr)
+    end
+  end
+end
 
 return M
