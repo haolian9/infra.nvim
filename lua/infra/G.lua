6bb19eee2cbbba9ebbd135d830d83dca57d0f994
haoliang <haoliang0.1.2@gmail.com>
2024-05-17 15:00:38 +0800

    sync with upstream

--- lua/infra/G.lua
+++ lua/infra/G.lua
@@ -1,3 +1,12 @@
+---concerns:
+---* default value
+---* nested dict
+---
+---usage
+---* on the user side: `local g = require'infra.G'(mod); g.x = y`
+---* on the plug/mod side: `mod/g.lua`, `return require'infra.G'(mod)`
+---
+
 ---@param ns string @namespace: a plugin name
 ---@return table
 return function(ns)
