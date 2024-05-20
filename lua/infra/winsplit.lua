db26ab5c2fc9110def9f1c199b96a0e824ac87b2
haoliang <haoliang0.1.2@gmail.com>
2024-05-20 22:30:41 +0800

    sync with upstream

--- lua/infra/winsplit.lua
+++ lua/infra/winsplit.lua
@@ -2,20 +2,20 @@ local api = vim.api
 
 ---@alias infra.winsplit.Side 'above'|'below'|'left'|'right'
 
-local cmds = {
-  above = { "split", "aboveleft" },
-  below = { "split", "belowright" },
-  left = { "vsplit", "aboveleft" },
-  right = { "vsplit", "belowright" },
-}
-
 ---split current window, put new window according to the 'side' param
 ---@param side infra.winsplit.Side
----@param path? string @a path or bufname
-return function(side, path)
-  local cmd, split = unpack(assert(cmds[side]))
-  local args = { path }
+---@param name_or_nr? string|integer @bufname or bufnr
+return function(side, name_or_nr)
+  local winid = api.nvim_get_current_win()
 
-  api.nvim_cmd({ cmd = cmd, mods = { split = split }, args = args }, { output = false })
+  ---just split
+  if name_or_nr == nil then return api.nvim_open_win(0, true, { split = side, win = winid }) end
+
+  ---split with bufnr
+  if type(name_or_nr) == "number" then return api.nvim_open_win(name_or_nr, true, { split = side, win = winid }) end
+
+  ---split with bufname
+  assert(type(name_or_nr) == "string" and name_or_nr ~= "")
+  api.nvim_open_win(vim.fn.bufnr(name_or_nr, true), true, { split = side, win = winid })
 end
 
